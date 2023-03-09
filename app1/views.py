108a22c2dec1fe39099fb0f921bad3b26e3a2d0b
saikumar5marripudi2 <saikumarmarmmarpudi8@gamil.com>
2023-03-09 19:45:37 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def sai(request):
+    return HttpResponse (' sai Is very good person')
---
+++ app2/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def Tsenam(request):
+    return HttpResponse('Tsenam is very smart person')
